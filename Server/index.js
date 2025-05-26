b4ec32cf97c20ac2e1e5bd2a7ef3e0167d3c2b6f
Srinithi <srinithi.tec@gmail.com>
2025-05-26 12:33:02 +0530

    Added link properly

--- Server/index.js
+++ Server/index.js
@@ -7,12 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "https://pixel-verse-eight.vercel.app/", // frontend URL
+  origin: "https://pixel-verse-eight.vercel.app", // frontend URL
 }));
 app.use(express.json());
 
 app.use("/api/email", emailRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
